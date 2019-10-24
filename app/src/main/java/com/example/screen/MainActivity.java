e36fbe2dcc6e17b434e9015f431232d57750a97b
zffjzwcfljj <1601950538@qq.com>
2019-10-24 10:03:43 +0800

    first commit

---
+++ app/src/main/java/com/example/screen/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+    }
+}
